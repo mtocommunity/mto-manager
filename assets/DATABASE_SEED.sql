a172cb325af44c24a6541231347fb2e18401d40e
Luis Bazán <luis@bazan.pe>
2024-08-01 19:55:08 -0500

    Update discord user code length

--- assets/DATABASE_SEED.sql
+++ assets/DATABASE_SEED.sql
@@ -1,7 +1,7 @@
 USE utp_community;
 
 CREATE TABLE user (
-    user_id CHAR(18) PRIMARY KEY,
+    user_id CHAR(20) PRIMARY KEY,
     user_code CHAR(9) NOT NULL UNIQUE,
     email CHAR(20) NOT NULL UNIQUE,
     username VARCHAR(20) NOT NULL,
@@ -9,7 +9,7 @@ CREATE TABLE user (
 );
 
 CREATE TABLE verification_code (
-  user_id CHAR(18) PRIMARY KEY,
+  user_id CHAR(20) PRIMARY KEY,
   code CHAR(6) NOT NULL,
   created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   FOREIGN KEY (user_id) REFERENCES user(user_id)
