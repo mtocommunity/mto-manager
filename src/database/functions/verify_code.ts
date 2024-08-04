c5d12f2359257157eb56235aefd8afb8b8cb2ac5
Luis Bazán <luis@bazan.pe>
2024-08-04 12:56:28 -0500

    Update function of database

--- src/database/functions/verify_code.ts
+++ src/database/functions/verify_code.ts
@@ -3,18 +3,18 @@ import VerifyCode from '../models/verify_code';
 export async function CreateCode(user_id: string, code: string) {
   const now = Date.now();
   await VerifyCode.create({
-    user_id: user_id,
+    discord_id: user_id,
     code: code,
     create_at: now
   });
 }
 
 export async function ExistCode(user_id: string) {
-  const exist = await VerifyCode.findOne({ where: { user_id } });
+  const exist = await VerifyCode.findOne({ where: { discord_id: user_id } });
   if (!exist) return null;
   return exist;
 }
 
 export async function DeleteCode(user_id: string) {
-  await VerifyCode.destroy({ where: { user_id } });
+  await VerifyCode.destroy({ where: { discord_id: user_id } });
 }
