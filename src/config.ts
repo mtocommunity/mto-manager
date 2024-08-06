ca011d2d95556ed27206ddae4d0f6e30f1ed0bbd
Luis Bazán <luis@bazan.pe>
2024-08-05 20:10:51 -0500

    Move IConfig to types files

--- src/config.ts
+++ src/config.ts
@@ -1,24 +1,23 @@
 import 'dotenv/config';
-import IConfig from '../IConfig';
+import IConfig from './ts/types/IConfig';
 
 const Config: IConfig = {
-    MAILSENDER: {
-        APIKEY: process.env.MAILSENDER_APIKEY ?? '',
-        FROM_EMAIL: process.env.MAILSENDER_FROM_EMAIL ?? '',
-        FROM_PASSWORD: process.env.MAILSENDER_FROM_PASSWORD ?? ''
-    },
-    DB: {
-        NAME: process.env.DB_NAME ?? '',
-        HOST: process.env.DB_HOST ?? '',
-        PORT: parseInt(process.env.DB_PORT ?? '3306') ?? 3306,
-        USERNAME: process.env.DB_USERNAME ?? '',
-        PASSWORD: process.env.DB_PASSWORD ?? '',
-        DATABASE: process.env.DB_DATABASE ?? ''
-    },
-    COMMUNITY_GUILD: {
-        unverifyRole: '1261606735471181854',
-        colaboratorRole: '1261157414099615784'
-    }
+  MAILSENDER: {
+    APIKEY: process.env.MAILSENDER_APIKEY ?? '',
+    FROM_EMAIL: process.env.MAILSENDER_FROM_EMAIL ?? '',
+    FROM_PASSWORD: process.env.MAILSENDER_FROM_PASSWORD ?? ''
+  },
+  DB: {
+    HOST: process.env.DB_HOST ?? '',
+    PORT: parseInt(process.env.DB_PORT ?? '3306') ?? 3306,
+    USERNAME: process.env.DB_USERNAME ?? '',
+    PASSWORD: process.env.DB_PASSWORD ?? '',
+    DATABASE: process.env.DB_DATABASE ?? ''
+  },
+  COMMUNITY_GUILD: {
+    unverifyRole: '1261606735471181854',
+    colaboratorRole: '1261157414099615784'
+  }
 };
 
 export default Config;
---
+++ src/ts/types/IConfig.ts
@@ -0,0 +1,18 @@
+export default interface IConfig {
+  MAILSENDER: {
+    APIKEY: string;
+    FROM_EMAIL: string;
+    FROM_PASSWORD: string;
+  };
+  DB: {
+    HOST: string;
+    PORT: number;
+    USERNAME: string;
+    PASSWORD: string;
+    DATABASE: string;
+  };
+  COMMUNITY_GUILD: {
+    unverifyRole: string;
+    colaboratorRole: string;
+  };
+}
